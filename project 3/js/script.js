a1f90a0e6c93cf6d35ce7757785a0d5ae9af176f
Lydia <ldgravy@gmail.com>
2022-03-28 14:08:49 -0400

    Prototype: created buttons to navigate timestamps

--- project 3/js/script.js
+++ project 3/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Project 3 Prototype
+Lydia Graveline
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -9,3 +9,26 @@ author, and this description to match your project!
 "use strict";
 
 // Code goes here
+
+let vid = document.getElementById("video");
+let btn = document.getElementById("button");
+
+// Go to video timestamp indicated by a button
+function setCurTime(timeValue) {
+  vid.currentTime = timeValue;
+  vid.play();
+}
+
+// listen on the event
+vid.addEventListener("timeupdate", function () {
+  // check whether we have passed 5 seconds,
+  // current time is given in seconds
+  if (this.currentTime >= 5 && !this.currentTime <= 5) {
+    // pause the playback
+    this.pause();
+  }
+});
+
+function play(){
+  vid.play();
+}
