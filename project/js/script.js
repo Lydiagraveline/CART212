42dc610b61eeb629238dc691aa787ea5b4e92821
Lydia <ldgravy@gmail.com>
2022-04-07 03:09:31 -0400

    added timestamps
    no video

--- project/js/script.js
+++ project/js/script.js
@@ -11,6 +11,7 @@ author, and this description to match your project!
 // Code goes here
 
 let vid = document.getElementById("video");
+let music = document.getElementById("music");
 let btn = document.getElementById("buttonBox");
 let option1 = document.getElementById("option1");
 let option2 = document.getElementById("option2");
@@ -22,6 +23,7 @@ function start(){
   document.getElementById("vidBox").style.display = "block";
   document.getElementById("startbtn").style.display = "none";
   vid.play();
+  music.play();
 }
 
 // Go to video timestamp indicated by a button
@@ -30,29 +32,80 @@ function setCurTime(timeValue) {
   play();
 }
 
+//loop the soundtrack
+music.addEventListener("timeupdate", function () {
+  if (music.currentTime >= 105){
+    music.currentTime = 3
+    music.play();
+  }
+});
+
 // listen on the event
 vid.addEventListener("timeupdate", function () {
   // pause the video if a decision is being made
   if (deciding === "true"){
-    this.pause();
-    btn.style.visibility = "visible"; //display the buttons
+    //this.pause();
+    option1.style.visibility = "visible"; //display the buttons
+    option2.style.visibility = "visible";
   }
 
-  if (this.currentTime >= 2 && this.currentTime <= 2.3 ) {
-    option1.setAttribute('onclick','play()')
-    option2.setAttribute('onclick','setCurTime(8)')
+
+  // timestamp 1
+  if (this.currentTime >= 4.5 && this.currentTime <= 9 ) {
+    //loop
+    if (this.currentTime >= 8.5){
+      this.currentTime = 5.5;
+    }
+    //skip forward
+    option1.setAttribute('onclick','setCurTime(76)')
+    //play
+    option2.setAttribute('onclick','setCurTime(9.3)')
     deciding = "true"
   }
-  if (this.currentTime >= 16) {
-    option1.setAttribute('onclick','setCurTime(0)')
-    option2.setAttribute('onclick','setCurTime(8)')
+  if (this.currentTime >= 25 && this.currentTime <= 33.5 ) {
+    //loop
+    if (this.currentTime >= 33){
+      this.currentTime = 28.3;
+    }
+    option1.setAttribute('onclick','setCurTime(122.5)') //skip forward
+    option2.setAttribute('onclick','setCurTime(34)') //play
     deciding = "true"
+  }
+  if (this.currentTime >= 56.5 && this.currentTime < 66.3 ) {
+    //loop
+    if (this.currentTime >= 66){
+      this.currentTime = 60;
+    }
+    option1.setAttribute('onclick','setCurTime(92.7)') //skip forward
+    option2.setAttribute('onclick','setCurTime(66.3)') //play
+    deciding = "true"
+  }
+
+  //restart from begining
+  if (this.currentTime >= 75 && this.currentTime < 75.3){
+    this.currentTime = 0;
+  }
+  //1 minute 46 seconds
+  if (this.currentTime >= 106.5 && this.currentTime <= 106.8 ) {
+    this.pause();
+    option1.setAttribute('onclick','setCurTime(152)') //skip forward
+    option2.setAttribute('onclick','setCurTime(107.1)') //play
+    deciding = "true"
+  }
+
+  //restart from begining
+  if (this.currentTime >= 151 && this.currentTime < 151.5){
+    this.currentTime = 0;
+  }
+  if (this.currentTime >= 176) {
+    this.currentTime = 0;
   }
 });
 
 // Play the video
 function play(){
   deciding = "false"
-  btn.style.visibility = "hidden";
+  option1.style.visibility = "hidden";
+  option2.style.visibility = "hidden";
   vid.play();
 }
